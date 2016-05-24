0f00f7400311dc76386d3fd763a847b030053e50
Max Britvin <maksbritvin@gmail.com>
2016-05-24 16:02:33 +0300

    Update EasyWay.py

--- EasyWay.py
+++ EasyWay.py
@@ -1,2 +1,10 @@
-for i in range(0,int(input())):
-	
+for i in range(1,int(input())):
+    if i % 15 == 0:
+        print ("FizzBuzz")
+    elif i % 5 == 0:
+        print ("Buzz")
+    elif i % 3 == 0:
+        print ("Fizz")
+    else:
+        print (i)
+
